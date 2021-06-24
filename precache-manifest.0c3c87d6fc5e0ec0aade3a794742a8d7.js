f3db7994f535205fed19fcc7891fd8f5b089af57
Vitaliy <kapron4ik@mail.ru>
2021-06-24 05:54:43 +0300

    Updates

---
+++ precache-manifest.0c3c87d6fc5e0ec0aade3a794742a8d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780e462a56d44547fa67b382e287f76d",
+    "url": "/IT_INCUBATOR_TodoList_TS/index.html"
+  },
+  {
+    "revision": "0c9b29c6a2e5e72449d3",
+    "url": "/IT_INCUBATOR_TodoList_TS/static/css/main.f0ac4d57.chunk.css"
+  },
+  {
+    "revision": "3821401a4f7b4c27c058",
+    "url": "/IT_INCUBATOR_TodoList_TS/static/js/2.13da4d6a.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/IT_INCUBATOR_TodoList_TS/static/js/2.13da4d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c9b29c6a2e5e72449d3",
+    "url": "/IT_INCUBATOR_TodoList_TS/static/js/main.553c4853.chunk.js"
+  },
+  {
+    "revision": "b17e98bfc5c71c3c5610",
+    "url": "/IT_INCUBATOR_TodoList_TS/static/js/runtime-main.e1c8db96.js"
+  }
+]);
